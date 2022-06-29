7e873984a06a853d9cc57e089d890f9cf8eac131
jooooonis <xowns3213@naver.com>
2022-06-29 11:54:42 +0900

    Chore : Local 서버 사용 mock서버 대신 Local 서버를 사용합니다.

--- src/components/elements/urls.js
+++ src/components/elements/urls.js
@@ -1,5 +1,5 @@
 export const SERVER_URL = {
   PRODUCT: 'https://e4d39e2c-535d-4181-b836-94891bccc69c.mock.pstmn.io',
   USER: 'https://03adbca0-7e7a-4d4f-be59-e432880d0b5d.mock.pstmn.io',
-  NEW: 'https://52ecacf6-6762-42db-9c38-6d8c411b81b9.mock.pstmn.io',
+  NEW: 'http://172.30.1.18:8000',
 };
