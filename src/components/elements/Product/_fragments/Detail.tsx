631bdc1604a37f6a9f79809b999fac508d889087
jooooonis <xowns3213@naver.com>
2022-07-05 16:57:40 +0900

    feat : 상품 상세 페이지 구매 팝업 추가 상품 상세페이지에서도 구매 팝업을 열 수 있습니다.

--- src/components/elements/Product/_fragments/Detail.tsx
+++ src/components/elements/Product/_fragments/Detail.tsx
@@ -11,12 +11,18 @@ import {
   AccordionPanel,
   Box,
   Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
   Flex,
   HStack,
   Image,
+  Input,
   Select,
   Text,
   VStack,
+  useDisclosure,
 } from '@chakra-ui/react';
 
 import { priceToString } from '@components/hooks';
@@ -149,6 +155,32 @@ function Detail() {
     }
   }, [ordering, hasPhoto]);
 
+  const [quantity, setQunatity] = useState(1);
+  const decQuantity = () => {
+    if (quantity > 1) {
+      setQunatity((quantity: number) => quantity - 1);
+    }
+  };
+
+  const incQuantity = () => {
+    if (quantity < 10) {
+      setQunatity((quantity: number) => quantity + 1);
+    }
+  };
+
+  const postCart = () => {
+    const url = SERVER_URL.LOCAL + '/v1/users/cart';
+    axios
+      .post(url, {
+        user: 1,
+        product: detail?.id,
+        quantity: quantity,
+      })
+      .then((res) => console.log(res));
+  };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box pt="120px" pb="80px">
       {detail && (
@@ -246,10 +278,11 @@ function Detail() {
                   {...ButtonStyle}
                   variant="outline"
                   colorScheme="primary"
+                  onClick={onOpen}
                 >
                   장바구니
                 </Button>
-                <Button {...ButtonStyle} colorScheme="primary">
+                <Button {...ButtonStyle} colorScheme="primary" onClick={onOpen}>
                   바로구매
                 </Button>
               </Flex>
@@ -378,6 +411,140 @@ function Detail() {
                 <SingleReview key={review.id} review={review} />
               ))}
           </Box>
+          <Drawer placement="bottom" onClose={onClose} isOpen={isOpen}>
+            <DrawerOverlay />
+            <DrawerContent bg="transparent">
+              <DrawerBody px="16px" py="20px" bg="white" borderTopRadius="20px">
+                <Box>
+                  <VStack
+                    alignItems="flex-start"
+                    p="10px"
+                    w="full"
+                    bg="gray.200"
+                    borderRadius="5px"
+                  >
+                    <Box {...SubText}>{detail?.name}</Box>
+                    <Flex justify="space-between" w="full" mt="4px">
+                      <Flex h="25px" alignSelf="center">
+                        <Box
+                          position="relative"
+                          bg="white"
+                          border="1px solid #EAECF0"
+                          borderRadius="5px 0px 0px 5px"
+                          p={0}
+                          w="25px"
+                          h="25px"
+                          _after={{
+                            content: '""',
+                            display: 'block',
+                            height: '1px',
+                            width: '9px',
+                            backgroundColor: '#4A4D55',
+                            position: 'absolute',
+                            top: '11px',
+                            left: '7px',
+                          }}
+                          _hover={{ cursor: 'pointer' }}
+                          onClick={decQuantity}
+                        ></Box>
+                        <Flex
+                          w="23px"
+                          h="full"
+                          borderTop="1px solid #EAECF0"
+                          borderBottom="1px solid #EAECF0"
+                        >
+                          <Input
+                            w="full"
+                            h="full"
+                            border="none"
+                            fontSize="12px"
+                            textAlign="center"
+                            color="gray.800"
+                            p={0}
+                            bg="white"
+                            value={quantity}
+                            readOnly
+                          ></Input>
+                        </Flex>
+                        <Box
+                          position="relative"
+                          bg="white"
+                          border="1px solid #EAECF0"
+                          borderRadius="0px 5px 5px 0px"
+                          w="25px"
+                          h="25px"
+                          p={0}
+                          _before={{
+                            content: '""',
+                            display: 'block',
+                            width: '1px',
+                            height: '9px',
+                            backgroundColor: '#4A4D55',
+                            position: 'absolute',
+                            top: '7px',
+                            left: '11px',
+                          }}
+                          _after={{
+                            content: '""',
+                            display: 'block',
+                            height: '1px',
+                            width: '9px',
+                            backgroundColor: '#4A4D55',
+                            position: 'absolute',
+                            top: '11px',
+                            left: '7px',
+                          }}
+                          onClick={incQuantity}
+                          _hover={{ cursor: 'pointer' }}
+                        ></Box>
+                      </Flex>
+                      <Flex {...TitleText} color="gray.600" alignItems="center">
+                        {priceToString(detail?.price)}원
+                      </Flex>
+                    </Flex>
+                  </VStack>
+                  <Flex justify="space-between" w="full" pt="15px">
+                    <Box {...SubText}>
+                      총 수량
+                      <span style={{ color: '#FF710B' }}> {quantity}</span> 개
+                    </Box>
+                    <Box {...SubText}>
+                      합계
+                      <span style={{ fontWeight: '700' }}>
+                        {' '}
+                        {priceToString(quantity * detail.price)}
+                      </span>
+                      원
+                    </Box>
+                  </Flex>
+                  <Flex justify="space-between" w="100%" pt="15px" pb="10px">
+                    <Button
+                      colorScheme="primary"
+                      w="165px"
+                      h="50px"
+                      borderRadius="25px"
+                      size="sd"
+                      py="12px"
+                    >
+                      바로구매
+                    </Button>
+                    <Button
+                      variant="outline"
+                      colorScheme="primary"
+                      w="165px"
+                      h="50px"
+                      borderRadius="25px"
+                      size="sd"
+                      py="12px"
+                      onClick={postCart}
+                    >
+                      장바구니
+                    </Button>
+                  </Flex>
+                </Box>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
         </>
       )}
     </Box>
