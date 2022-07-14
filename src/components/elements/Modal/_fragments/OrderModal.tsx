306573d1c664c9648b4303cbabae6ad77ed6bbc9
jooooonis <xowns3213@naver.com>
2022-07-14 16:49:44 +0900

    feat : 주문최소 성공 확인 모달 구현 주문취소가 되면 확인 모달을 추가로 띄웁니다.

--- src/components/elements/Modal/_fragments/OrderModal.tsx
+++ src/components/elements/Modal/_fragments/OrderModal.tsx
@@ -6,6 +6,7 @@ import {
   ModalCloseButton,
   ModalContent,
   ModalOverlay,
+  useDisclosure,
 } from '@chakra-ui/react';
 
 import instance from '@apis/_axios/instance';
@@ -21,17 +22,54 @@ interface OrderModalProps extends MyModalProps {
   onClose: () => void;
   merchantUid: string;
 }
+function OrderCanceledModal({ isOpen, onClose }: MyModalProps) {
+  return (
+    <>
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent w="343px">
+          <ModalBody pt={0} px="11px" pb="33px">
+            <Box {...MessageStyle} textAlign="center" pt="110px" pb="80px">
+              주문이 취소가 완료되었습니다.
+            </Box>
+            <Flex justify="center">
+              <PrimaryButton onClick={onClose} w="155px">
+                확인
+              </PrimaryButton>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 function OrderModal({ isOpen, onClose, merchantUid }: OrderModalProps) {
+  const {
+    isOpen: isOrderCanceledModalOpen,
+    onOpen: onOrderCanceledModalOpen,
+    onClose: onisOrderCanceledModalClose,
+  } = useDisclosure();
+
   const cancelOrder = () => {
-    instance.post('/v1/orders/payment/cancel', {
-      merchantUid: merchantUid,
-      reason: '주문최소',
-    });
+    instance
+      .post('/v1/orders/payment/cancel', {
+        merchantUid: merchantUid,
+        reason: '주문최소',
+      })
+      .then((res) => {
+        console.log(res.data);
+        // api 수정되면 onClose(), onOrderCanceledModalOpen() 여기로 옮겨주세요
+      });
     onClose();
+    onOrderCanceledModalOpen();
   };
   return (
     <>
+      <OrderCanceledModal
+        isOpen={isOrderCanceledModalOpen}
+        onClose={onisOrderCanceledModalClose}
+      />
       <Modal
         onClose={onClose}
         isOpen={isOpen}
