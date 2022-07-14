6961976a057edc7ea8c7d7b50b722abbd6f3f232
jooooonis <xowns3213@naver.com>
2022-07-14 12:43:30 +0900

    feat : 장바구니 모달 퍼블리싱

--- src/components/elements/Modal/_fragments/CartModal.tsx
+++ src/components/elements/Modal/_fragments/CartModal.tsx
@@ -1,10 +1,12 @@
 import {
+  Box,
   Button,
   Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalHeader,
   ModalOverlay,
 } from '@chakra-ui/react';
 
@@ -15,20 +17,36 @@ interface MyModalProps {
   onClose: () => void;
 }
 
-export default function CartModal({ isOpen, onClose }: MyModalProps) {
+function CartModal({ isOpen, onClose }: MyModalProps) {
   return (
     <>
       <Modal onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
-        <ModalContent>
-          <ModalCloseButton />
-          <ModalBody>장바구니에 상품이 담겼습니다.</ModalBody>
-          <Flex>
-            <PrimaryButton onClick={onClose}>장바구니 이동</PrimaryButton>
-            <PrimaryButton onClick={onClose}>쇼핑 계속하기</PrimaryButton>
-          </Flex>
+        <ModalContent w="343px">
+          <ModalCloseButton size="lg" color="gray.600" top="25px" />
+          <ModalBody pt={0} px="11px" pb="33px">
+            <Box {...MessageStyle} textAlign="center" pt="110px" pb="80px">
+              장바구니에 상품이 담겼습니다.
+            </Box>
+            <Flex justify="space-between">
+              <PrimaryButton onClick={onClose} variant="outline" w="155px">
+                장바구니 이동
+              </PrimaryButton>
+              <PrimaryButton onClick={onClose} w="155px">
+                쇼핑 계속하기
+              </PrimaryButton>
+            </Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
   );
 }
+
+export default CartModal;
+
+const MessageStyle = {
+  fontWeight: 700,
+  fontSize: '16px',
+  lineHeight: '28px',
+};
