4ecddba5a55d042699723868c270d90426b3bedf
jooooonis <xowns3213@naver.com>
2022-07-15 10:23:50 +0900

    feat : 회원정보 수정 모달 구현

--- src/components/elements/Modal/index.ts
+++ src/components/elements/Modal/index.ts
@@ -1,6 +1,7 @@
 import CartModal from './_fragments/CartModal';
+import EditModal from './_fragments/EditModal';
 import Modal from './_fragments/Modal';
 import OrderModal from './_fragments/OrderModal';
 import ReviewModal from './_fragments/ReviewModal';
 
-export { Modal, CartModal, OrderModal, ReviewModal };
+export { Modal, EditModal, CartModal, OrderModal, ReviewModal };
