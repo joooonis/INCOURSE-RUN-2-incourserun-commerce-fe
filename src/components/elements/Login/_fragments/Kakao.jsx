d25bce4f15e718275623244faed6e3e11a2d331b
jooooonis <xowns3213@naver.com>
2022-07-15 12:05:28 +0900

    feat : 로그인 요청 api 주소 변경

--- src/components/elements/Login/_fragments/Kakao.jsx
+++ src/components/elements/Login/_fragments/Kakao.jsx
@@ -11,10 +11,9 @@ const Kakao = () => {
       .post('/v1/users/social_login', {
         code: code,
         state: 'kakao',
-        redirectUri: 'http://172.30.1.17:3000/login/kakao/callback',
+        redirectUri: 'http://incourserun.cf/login/kakao/callback',
       })
       .then((res) => {
-        // console.log(res);
         const token = res.data.access;
         const isRegister = res.data.isRegister;
 
