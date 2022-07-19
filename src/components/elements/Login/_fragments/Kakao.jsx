4db1ceb1f9bb1b6d6490a76aaac1121f85b917e8
jooooonis <xowns3213@naver.com>
2022-07-19 11:31:01 +0900

    fix : 최초로그인시에 회원가입 페이지로 이동

--- src/components/elements/Login/_fragments/Kakao.jsx
+++ src/components/elements/Login/_fragments/Kakao.jsx
@@ -24,7 +24,7 @@ const Kakao = () => {
         }
 
         if (isRegister) router.push('/');
-        else router.replace('/login');
+        else router.replace('/join');
       })
       .catch((err) => {
         console.log('소셜로그인 에러', err);
