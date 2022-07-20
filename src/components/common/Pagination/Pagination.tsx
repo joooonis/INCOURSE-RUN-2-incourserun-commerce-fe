538f06f1b6c293b38e9d70ad6adff7f0eeef0c12
jooooonis <xowns3213@naver.com>
2022-07-20 11:17:01 +0900

    feat : 페이지네이션 구현

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Button, Flex } from '@chakra-ui/react';
 
 interface PaginationProps {
@@ -9,32 +11,46 @@ interface PaginationProps {
 
 function Pagination({ total, limit, page, setPage }: PaginationProps) {
   const numPages = Math.ceil(total / limit);
-
+  const [selected, setSelected] = useState<boolean[]>([]);
+  const pageIndexComparsion = (num: number) => {
+    if (num % 5 == 0) return Math.ceil((num - 1) / 5);
+    else return Math.ceil(num / 5);
+  };
   return (
     <>
       <Flex py="30px" justifyContent="center" alignItems="center">
+        <Button
+          onClick={() => setPage(page - 1)}
+          display={page === 1 ? 'none' : 'block'}
+          variant="ghost"
+        >
+          &lt;
+        </Button>
         {Array(numPages)
           .fill(0)
-          .map((_, index) => (
-            <Button
-              variant="ghost"
-              key={index + 1}
-              onClick={() => setPage(index + 1)}
-              _hover={{ cursor: 'pointer' }}
-              _selected={{ color: '#1A1A1A' }}
-              display={
-                Math.ceil(page / 5) == Math.ceil(index / 5) ? 'block' : 'none'
-              }
-              {...TabStyle}
-            >
-              {index + 1}
-            </Button>
-          ))}
+          .map((_, index) => {
+            return (
+              <Button
+                variant="ghost"
+                key={index}
+                onClick={() => setPage(index + 1)}
+                _hover={{ cursor: 'pointer' }}
+                color={page == index + 1 ? '#1A1A1A' : 'gray.400'}
+                display={
+                  pageIndexComparsion(page) == pageIndexComparsion(index + 1)
+                    ? 'block'
+                    : 'none'
+                }
+                {...TabStyle}
+              >
+                {index + 1}
+              </Button>
+            );
+          })}
         <Button
           onClick={() => setPage(page + 1)}
-          disabled={page === numPages}
+          display={page === numPages ? 'none' : 'block'}
           variant="ghost"
-          {...TabStyle}
         >
           &gt;
         </Button>
@@ -49,5 +65,5 @@ const TabStyle = {
   fontWeight: 700,
   fontSize: '16px',
   lineHeight: '28px',
-  color: 'gray.400',
+  // color: 'gray.400',
 };
