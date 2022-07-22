7d22b797869c7a46f473b0e971e25cd2485ec072
jooooonis <xowns3213@naver.com>
2022-07-22 17:15:25 +0900

    feat : 모바일 결제 성공 시 모달을 띄우고 결제내역 페이지로 이동

--- src/components/elements/Order/index.ts
+++ src/components/elements/Order/index.ts
@@ -1,5 +1,5 @@
 import CartPayment from './_fragments/CartPaymentPage';
-import CompleteMobile from './_fragments/CompleteMobilePage';
+import CompleteMobile from './_fragments/CompleteMobile';
 import Complete from './_fragments/CompletePage';
 import Order from './_fragments/OrderPage';
 import Payment from './_fragments/PaymentPage';
